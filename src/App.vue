47ba5b6e9621a8fdc175378b0a6c9ce0b0cf61c0
yangfanghua <s8410646@gmail.com>
2023-05-08 14:25:09 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import area1 from "./components/Area.vue"
+export default {
+  components: {
+    area1}
+  }
+</script>
+
+<template>
+
+<area1 />
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Area.vue
@@ -0,0 +1,73 @@
+<script>
+export default {
+  components: {},
+  //資料變數宣告
+  data() {
+    return {
+        min:1,
+        max:100,
+        guess:null,
+        answer:Math.floor(Math.random()*100+1),
+        isans:false,
+    };
+  },
+  //方法
+  methods: {
+
+    fn() {
+        if (this.guess < this.min || this.guess > this.max) {
+          alert("看清楚~~!不要亂輸入拉");
+          this.guess=null;
+          return;
+        }
+        if (this.guess < this.answer) {
+          this.min = this.guess;
+          this.guess=null;
+        }
+        if (this.guess > this.answer) {
+            this.max = this.guess;
+            this.guess=null;
+        }
+        if (this.guess == this.answer) {
+          alert("答對拉~!");
+          this.isans=true;
+        }
+      },
+
+  },
+  //生命週期
+  mounted() {},
+};
+</script>
+
+<template>
+  <div class="cantainer bg-ligh">
+    <p class="h1" >猜猜看</p>
+    
+    <div v-if="isans">
+        <p>答對了</p>
+    </div>
+
+    <div class="number" v-else>
+    <p>{{ min }}</p>
+    <p>-</p>
+    <p>{{ max }}</p>
+    </div>
+    <input type="number" v-model="guess">
+    <button class="btn-secondary" type="button" @click="fn">回答</button>
+  </div>
+</template>
+
+<style scoped>
+
+.cantainer{
+text-align: center;
+
+}
+.number{
+    display: flex;
+    justify-content: center;
+    font-size: 180px;
+}
+
+</style>
